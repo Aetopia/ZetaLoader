e5615986a5e27ace777dc2781df896ddef0c7c2e
Aetopia <kirbymario22@gmail.com>
2022-12-18 20:08:36 +0530

    Use SW_FORCEMINIMIZE.

--- src/Zeta.c
+++ src/Zeta.c
@@ -69,7 +69,7 @@ void WndDMThreadProc(
         {
             wnd.cds = TRUE;
             if (!IsIconic(wnd.hwnd))
-                ShowWindow(wnd.hwnd, SW_MINIMIZE);
+                ShowWindow(wnd.hwnd, SW_FORCEMINIMIZE);
             if (!!wnd.dm.dmFields)
                 SetDM(0);
         };
