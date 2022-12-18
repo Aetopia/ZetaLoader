aabeb43b45e96833d7ff1ac37c851d0b32039e9c
Aetopia <kirbymario22@gmail.com>
2022-12-18 23:15:55 +0530

    Update Zeta.c

--- src/Zeta.c
+++ src/Zeta.c
@@ -98,7 +98,7 @@ DWORD SetWndPosThread()
         SetWindowPos(wnd.hwnd, HWND_TOPMOST,
                      wnd.mi.rcMonitor.left, wnd.mi.rcMonitor.top,
                      wnd.cx, wnd.cy,
-                     SWP_ASYNCWINDOWPOS | SWP_NOACTIVATE |
+                     SWP_NOACTIVATE |
                          SWP_NOSENDCHANGING |
                          SWP_NOOWNERZORDER |
                          SWP_NOZORDER);
