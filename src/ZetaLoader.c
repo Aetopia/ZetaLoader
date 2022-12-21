55fe3198d5e3c5408c9bf4393c4e385721c33ac4
Aetopia <kirbymario22@gmail.com>
2022-12-21 12:23:41 +0530

    Update ZetaLoader.c

--- src/ZetaLoader.c
+++ src/ZetaLoader.c
@@ -14,14 +14,17 @@ int main(__attribute__((unused)) int argc, char *argv[])
     GetFullPathName("Zeta.dll", MAX_PATH, dll, NULL);
     if (GetFileAttributes(dll) == INVALID_FILE_ATTRIBUTES || GetFileAttributes(proc) == INVALID_FILE_ATTRIBUTES)
         return 0;
-    CreateProcess(proc, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+    CreateProcess(proc, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
 
     // Inject Zeta.dll into Halo Infinite.
     mem = VirtualAllocEx(pi.hProcess, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     WriteProcessMemory(pi.hProcess, mem, (LPCVOID)dll, MAX_PATH, NULL);
 #pragma GCC diagnostic ignored "-Wcast-function-type"
-    CreateRemoteThread(pi.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, mem, 0, 0);
+    WaitForSingleObject(CreateRemoteThread(pi.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, mem, 0, 0), INFINITE);
 #pragma GCC diagnostic pop
     VirtualFreeEx(pi.hProcess, mem, MAX_PATH, MEM_RELEASE);
+
+    // Resume Halo Infinite.
+    ResumeThread(pi.hThread);
     return 0;
 }
