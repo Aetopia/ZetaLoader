996ede93ae1e1f63cdc94285ab5d3be3fab209fb
Aetopia <kirbymario22@gmail.com>
2022-12-21 21:53:54 +0530

    replace.

--- src/ZetaLoader.c
+++ src/ZetaLoader.c
@@ -6,25 +6,23 @@ int main(__attribute__((unused)) int argc, char *argv[])
     SetCurrentDirectory(dirname(argv[0]));
     LPVOID mem;
     char dll[MAX_PATH];
-    SHELLEXECUTEINFO ei = {.cbSize = sizeof(ei),
-                           .lpFile = "HaloInfinite.exe",
-                           .lpVerb = "open",
-                           .nShow = 5,
-                           .fMask = SEE_MASK_NOCLOSEPROCESS | SEE_MASK_NOASYNC};
+    STARTUPINFO si = {.cb = sizeof(si)};
+    PROCESS_INFORMATION pi;
 
     GetFullPathName("Zeta.dll", MAX_PATH, dll, NULL);
     if (GetFileAttributes(dll) == INVALID_FILE_ATTRIBUTES ||
         GetFileAttributes("HaloInfinite.exe") == INVALID_FILE_ATTRIBUTES)
         return 0;
-    if (!ShellExecuteEx(&ei))
+    if (!CreateProcess("HaloInfinite.exe", NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi))
         return 0;
 
     // Inject Zeta.dll into Halo Infinite.
-    mem = VirtualAllocEx(ei.hProcess, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    WriteProcessMemory(ei.hProcess, mem, (LPCVOID)dll, MAX_PATH, NULL);
+    mem = VirtualAllocEx(pi.hProcess, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    WriteProcessMemory(pi.hProcess, mem, (LPCVOID)dll, MAX_PATH, NULL);
 #pragma GCC diagnostic ignored "-Wcast-function-type"
-    WaitForSingleObject(CreateRemoteThread(ei.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, mem, 0, 0), INFINITE);
+    WaitForSingleObject(CreateRemoteThread(pi.hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, mem, 0, 0), INFINITE);
 #pragma GCC diagnostic pop
-    VirtualFreeEx(ei.hProcess, mem, MAX_PATH, MEM_RELEASE);
+    VirtualFreeEx(pi.hProcess, mem, MAX_PATH, MEM_RELEASE);
+    ResumeThread(pi.hThread);
     return 0;
 }
