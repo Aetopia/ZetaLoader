bd8a72a153eadfcde0987f22e22eb4e6275a0a27
Aetopia <kirbymario22@gmail.com>
2022-12-21 21:09:30 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 gcc -shared -s -Ofast src\Zeta.c -lshcore -ntdll -o Zeta.dll
-gcc -mwindows -s -Ofast src\ZetaLoader.c -o ZetaLoader.exe
+gcc -mwindows -s -O2 src\ZetaLoader.c -o ZetaLoader.exe
 :: Optional UPX compression.
 upx --best --ultra-brute Zeta.dll ZetaLoader.exe>nul 2>&1
