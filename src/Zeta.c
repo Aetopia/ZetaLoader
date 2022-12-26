c4f3f242325fdd7fb1c567eda7f3ffc4a3bf784f
Aetopia <kirbymario22@gmail.com>
2022-12-26 12:34:53 +0530

    Update Zeta.c

--- src/Zeta.c
+++ src/Zeta.c
@@ -98,15 +98,30 @@ DWORD Zeta()
     char *c, pri[CCHDEVICENAME];
     DEVMODE dm;
     HMONITOR hmon;
+    ULONG min, max, cur;
     UINT dpi;
     float scale;
+
+    /*
+    Force the highest timer resolution.
+    Halo Infinite uses 1 ms by default, we can force 0.5 ms using NtSetTimerResolution.
+    Starting with Windows 2004, setting the timer resolution is no longer global but on a per process basis.
+    Reference: https://learn.microsoft.com/en-us/windows/win32/api/timeapi/nf-timeapi-timebeginperiod#remarks
+    */
+    NtQueryTimerResolution(&min, &max, &cur);
+    NtSetTimerResolution(max, TRUE, &cur);
+
+    // Makes sure that the SetForegroundWindow() or any similar functions work properly.
+    wnd.pid = GetCurrentProcessId();
+    AllowSetForegroundWindow(wnd.pid);
+    SystemParametersInfo(SPI_SETFOREGROUNDLOCKTIMEOUT, 0, 0, SPIF_UPDATEINIFILE);
 
     // Get process ID and window HWND using IsPIDWnd.
     while (EnumWindows(EnumWindowsProc, 0))
         Sleep(1);
     do
         SwitchToThisWindow(wnd.hwnd, TRUE);
-    while (wnd.hwnd != GetForegroundWindow(wnd.hwnd));
+    while (wnd.hwnd != GetForegroundWindow());
 
     // Get the primary monitor.
     GetMonitorInfo(MonitorFromWindow(0, MONITORINFOF_PRIMARY), (MONITORINFO *)&wnd.mi);
@@ -187,25 +202,7 @@ BOOL WINAPI DllMain(__attribute__((unused)) HINSTANCE hInstDll,
                     __attribute__((unused)) LPVOID lpReserved)
 {
     if (fwdreason == DLL_PROCESS_ATTACH)
-    {
-
-        /*
-        Force the highest timer resolution.
-        Halo Infinite uses 1 ms by default, we can force 0.5 ms using NtSetTimerResolution.
-        Starting with Windows 2004, setting the timer resolution is no longer global but on a per process basis.
-        Reference: https://learn.microsoft.com/en-us/windows/win32/api/timeapi/nf-timeapi-timebeginperiod#remarks
-        */
-        ULONG min, max, cur;
-        NtQueryTimerResolution(&min, &max, &cur);
-        NtSetTimerResolution(max, TRUE, &cur);
-
-        // Makes sure that the SetForegroundWindow() or any similar functions work properly.
-        wnd.pid = GetCurrentProcessId();
-        AllowSetForegroundWindow(wnd.pid);
-        SystemParametersInfo(SPI_SETFOREGROUNDLOCKTIMEOUT, 0, 0, SPIF_UPDATEINIFILE);
-        
         // Execute Zeta() function in a thread to prevent the target application from getting locked up.
         CreateThread(0, 0, Zeta, NULL, 0, 0);
-    };
     return TRUE;
 }
