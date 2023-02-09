d2a11a548000779b9da403b5a3cbd597e5b457f4
Aetopia <kirbymario22@gmail.com>
2023-02-09 16:05:58 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,11 +5,11 @@ cd "%~dp0"
 nimpretty src/ZetaLoader.nim src/DumpTool.nim>nul 2>&1
 
 :: ZetaLoader's dynamic link library needs be in the "src" folder in ordered be embedded into the executable.
-nim c -d:release -d:strip --app:lib -d:strip -o:src/ZetaLoader.dll src/ZetaLoader.nim
+nim c --mm:arc -d:release -d:strip --app:lib -d:strip -o:src/ZetaLoader.dll src/ZetaLoader.nim
 upx --best --ultra-brute src/ZetaLoader.dll>nul 2>&1
 
 :: Compile ZetaLoader's injector with dynamic link library.
-nim c -d:release -d:strip --app:gui -o:DumpTool.exe src/DumpTool.nim
+nim c --mm:arc -d:release -d:strip --app:gui -o:DumpTool.exe src/DumpTool.nim
 upx --best --ultra-brute DumpTool.exe>nul 2>&1
 
 :: Cleanup.
