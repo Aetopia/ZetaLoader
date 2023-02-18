3532a0606c9e398a1bd1a7def2839a10181d2891
Aetopia <kirbymario22@gmail.com>
2023-02-18 11:50:16 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd "%~dp0"
 nimpretty src/main.nim>nul 2>&1
-nim c --mm:arc --opt:size -d:release -d:noRes -d:strip --app:lib -d:strip -o:dinput8.dll src/main.nim
+nim c --mm:arc --opt:size -d:release -d:noRes -d:strip --app:lib -o:dinput8.dll src/main.nim
 upx --best --ultra-brute dinput8.dll>nul 2>&1
