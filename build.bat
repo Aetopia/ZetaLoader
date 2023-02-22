c359073b20e127c01017f2890b7b3b75b4dcc489
Aetopia <kirbymario22@gmail.com>
2023-02-22 16:44:41 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd "%~dp0"
 nimpretty src/main.nim>nul 2>&1
-nim c -d:release -d:useMalloc --mm:arc --opt:size -d:noRes -d:strip --app:lib -o:dinput8.dll src/main.nim
+nim c --passC:-flto --passL:-flto -d:danger -d:useMalloc --mm:arc --opt:size -d:noRes -d:strip --app:lib -o:dinput8.dll src/main.nim
 upx --best --ultra-brute dinput8.dll>nul 2>&1
